c9f56f6c90f0436913c6c554e66449906ecbe254
topzz100 <adenekantope@gmail.com>
2022-08-23 13:04:15 +0100

    feat: add catgory page
    category page completed( filters and products section)

--- frontend/src/components/CategoryMain/CategoryMain.tsx
+++ frontend/src/components/CategoryMain/CategoryMain.tsx
@@ -3,29 +3,44 @@ import Brands from '../Brands/Brands'
 import ColorFilter from '../ColorFilter/ColorFilter'
 import Filters from '../Filters/Filters'
 import PriceFilter from '../PriceFilter/PriceFilter'
+import Product from '../Product/Product'
 import SizeFilter from '../SizeFilter/SizeFilter'
 import styles from './CategoryMain.module.css'
 
 const CategoryMain = () => {
+
+  const showProducts = () => {
+    let element = []
+    for (let index = 0; index < 20; index++) {
+      element.push(index)
+        
+     }
+     return element
+    }
   return (
     <div className={styles.categoryMain}>
-      <h4>
+      <h4 className={styles.filterTitle}>
         Filters
       </h4>
       <div className={styles.wrapper}>
 
         {/* left */}
         <div className={styles.left}>
-          <Filters/>
-          <Brands/>
-          <PriceFilter/>
-          <SizeFilter/>
-          <ColorFilter/> 
-          <Availability/>
+          <div className={styles.leftCon}>
+            <Filters/>
+            <Brands/>
+            <PriceFilter/>
+            <SizeFilter/>
+            <ColorFilter/> 
+            <Availability/>
+          </div>
+          
         </div>
         {/* right */}
         <div className={styles.right}>
-          right
+          {showProducts().map((e, i) => (
+            <Product key={i} />
+          ))}
         </div>
       </div>
     </div>
